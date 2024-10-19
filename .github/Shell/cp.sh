685f835aae827dd4cd0fd2a05aba79177fc73fb0
3wlh <1094890624@qq.com>
2024-10-19 19:39:36 +0800

    更新 cp.sh

--- .github/Shell/cp.sh
+++ .github/Shell/cp.sh
@@ -2,5 +2,5 @@
 Package_dir="${1}"
 Save_dir="${2}"
 Arch="${3}"
-Name=$(echo ${Package_dir} | sed 's|.*/||g' | sed "s|_all|_${Arch}|")
+Name=${Arch}_$(echo ${Package_dir} | sed 's|.*/||g')
 cp -f "${Package_dir}" "${Save_dir}/${Name}"
