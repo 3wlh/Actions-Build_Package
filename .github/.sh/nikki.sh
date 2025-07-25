cd8f39e44bde4214f0641ff1750dda27b66e280b
3wlh <1094890624@qq.com>
2025-07-26 01:48:01 +0800

    Create nikki.sh

---
+++ .github/.sh/nikki.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 下载 luci-app-nikki ..."
+Data="$(curl -s https://api.github.com/repos/nikkinikki-org/OpenWrt-nikki/releases/latest)"
+gz_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*'${1}'-openwrt-24.10.tar.gz"' | cut -d '"' -f 4)"
+echo "Downloading ${gz_url}"
+if [[ "$(du -b "$(pwd)/packages/diy_packages/$(basename ${gz_url})" 2>/dev/null | awk '{print $1}')" -ge "6000" ]]; then
+	echo "######################################################################## 100.0%"
+else	
+	find $(pwd)/packages/diy_packages/ -type f -name "$(basename ${gz_url})" -exec rm -f {} \;
+	curl -# -L --fail "${gz_url}" -o "$(pwd)/packages/diy_packages/$(basename ${gz_url})"
+    # #wget -qO "$(pwd)/packages/diy_packages/$(basename $Download_URL)" "${Download_URL}" --show-progress
+fi
+find $(pwd)/packages/diy_packages/ -type f -name "$(basename ${gz_url})" -exec tar -zxf {} -C "$(pwd)/packages/diy_packages/" \;
+# find $(pwd)/packages/diy_packages -type f -name "*.[ia]pk" -exec mv -f {} "$(pwd)/packages/diy_packages/" \;
