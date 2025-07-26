465d13261f87595b4b765c20e8c476adec21c7be
3wlh <1094890624@qq.com>
2025-07-26 08:55:17 +0800

    up

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-# find "$(pwd)/dl" -type f
-cat "$(pwd)/repositories.conf" | \
+cat "${3}/releases.txt" | \
 while IFS= read -r LINE; do
-    [[ -z "$(echo "${LINE}" | grep -Eo "^src/gz")" ]] && continue
-    name=$(echo "${LINE}" | cut -d " " -f 2)
-    url=$(echo "${LINE}" | cut -d " " -f 3)
-    [[ -z "${name}" || -z "${url}" ]] && continue
-    # echo -e "检查${name}更新：" 
-    echo "Downloading ${url}/Packages.gz"
-    curl -# --fail "${url}/Packages.gz" -o "/tmp/Packages.gz"
-    md5url=$(find "/tmp/" -type f -name "Packages.gz" 2>/dev/null -exec md5sum -b {} \; | awk '{print $1}')
-    md5name=$(find "$(pwd)/dl" -type f -name "${name}" 2>/dev/null -exec md5sum -b {} \; | awk '{print $1}')
-    echo "插件${name}:{\"md5url\": \"${md5url}\",\"md5name\": \"${md5name}\"}"
-    if [[ "${md5url}" == "${md5name}" ]]; then
-        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m -【${name}】无更新插件.\e[0m\n"
-    else
-        # 删除 GitHub 缓存
-        rm -rf "$(pwd)/dl/*"
-        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m -【${name}】有更新插件.\e[0m"
-        echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 删除所有缓存插件！\e[0m\n"
-        echo "cache=delete" >> "$(pwd)/bin/.bashrc"
-        break
-    fi
+    [[ -z "${1}" || -z "${2}" || -z "${LINE}" ]] && continue
+    [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" ]] && continue
+    data=${LINE} 
 done
+[[ -z "${data}" ]] && echo ${2} && echo "${1} ${2}" >>${3}/releases.txt 
+[[ -n "${data}" ]] && name=$(echo "${data}" | cut -d " " -f 1)
+[[ -n "${data}" ]] && url=$(echo "${data}" | cut -d " " -f 2)
+if [[ -n "${data}" && "${2}" != "${url}" ]]; then
+    echo ${2}
+    sed -i "s|${url}|${2}|" ${3}/releases.txt
+fi
--- .github/.sh/Nikki.sh
+++ .github/.sh/Nikki.sh
@@ -2,13 +2,15 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 下载 luci-app-nikki ..."
 Data="$(curl -s https://api.github.com/repos/nikkinikki-org/OpenWrt-nikki/releases/latest)"
 gz_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*'${1}'-openwrt-24.10.tar.gz"' | cut -d '"' -f 4)"
+Check=$(Check "nikki" "${gz_url}" "${2}")
+[[ -z ${Check} ]] && echo "【nikki】- 插件未更新" && exit 0 >/dev/null
 echo "Downloading ${gz_url}"
 if [[ "$(du -b "$(pwd)/packages/diy_packages/$(basename ${gz_url})" 2>/dev/null | awk '{print $1}')" -ge "6000" ]]; then
 	echo "######################################################################## 100.0%"
 else	
 	find $(pwd)/packages/diy_packages/ -type f -name "$(basename ${gz_url})" -exec rm -f {} \;
 	curl -# -L --fail "${gz_url}" -o "$(pwd)/packages/diy_packages/$(basename ${gz_url})"
-    # #wget -qO "$(pwd)/packages/diy_packages/$(basename $Download_URL)" "${Download_URL}" --show-progress
+    # #wget -qO "$(pwd)/packages/diy_packages/$(basename $Download_URL)" "${d_URL}" --show-progress
 fi
 find $(pwd)/packages/diy_packages/ -type f -name "$(basename ${gz_url})" -exec tar -zxf {} -C "$(pwd)/packages/diy_packages/" \;
 # find $(pwd)/packages/diy_packages -type f -name "*.[ia]pk" -exec mv -f {} "$(pwd)/packages/diy_packages/" \;
