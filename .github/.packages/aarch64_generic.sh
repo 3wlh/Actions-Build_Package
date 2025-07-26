cdfad6f7aa0bfbc1409e7dac250a160c652ab53e
3wlh <1094890624@qq.com>
2025-07-26 13:24:08 +0800

    up

--- .github/.packages/aarch64_generic.sh
+++ .github/.packages/aarch64_generic.sh
@@ -2,8 +2,8 @@
 Segmentation "https://dl.openwrt.ai/releases/24.10/packages/aarch64_generic/kiddin9/" \
 "${1}/packages/aarch64_generic" \
 "luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel \
-taskd luci-lib-xterm luci-lib-taskd luci-app-store"
+taskd luci-lib-xterm luci-lib-taskd luci-app-store luci-app-socat"
 Passwall "aarch64_generic" "24.10" "${1}"
 Openlist2 "aarch64_generic" "${1}"
 Nikki "aarch64_generic" "${1}"
-Socat "aarch64_generic" "${1}"
+# Socat "aarch64_generic" "${1}"
--- .github/.packages/x86_64.sh
+++ .github/.packages/x86_64.sh
@@ -1,8 +1,8 @@
 Segmentation "https://dl.openwrt.ai/releases/24.10/packages/x86_64/kiddin9/" \
 "${1}/packages/x86_64" \
 "luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel \
-taskd luci-lib-xterm luci-lib-taskd luci-app-store"
+taskd luci-lib-xterm luci-lib-taskd luci-app-store luci-app-socat"
 Passwall "x86_64" "24.10" "${1}"
 Openlist2 "x86_64" "${1}"
 Nikki "x86_64" "${1}"
-Socat "x86_64" "${1}"
+# Socat "x86_64" "${1}"
