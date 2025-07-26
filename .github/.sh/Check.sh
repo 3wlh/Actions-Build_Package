07f6fcacc07f7effa10c2faebbf1ffc0d85ec633
3wlh <1094890624@qq.com>
2025-07-26 11:12:07 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test -f "${3}releases.txt" || touch "${3}releases.txt"
 function modify(){
-cat "${3}releases.txt" | \
+cat "${3}" | \
 while IFS= read -r LINE; do
     [[ -z "$(echo "${LINE}" | grep -Eo "^Package:${1}.*")" ]] && continue
     url=$(echo "${LINE}" | cut -d " " -f 2)
@@ -14,7 +14,7 @@ done
 }
 Releases=$(cat "${3}releases.txt" 2>/dev/null)
 if [[ "${Releases}" =~ "Package:${1}" ]]; then
-    modify "Package:${1}" "${2}"
+    modify "Package:${1}" "${2}" "${3}releases.txt"
 else
 	echo "update" && echo "Package:${1} ${2}" >>"${3}releases.txt"
 fi
