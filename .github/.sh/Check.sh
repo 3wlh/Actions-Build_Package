7b1d188552837a0f24ee8dd32a6f99f276fe961f
3wlh <1094890624@qq.com>
2025-07-26 09:58:58 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat "${3}releases.txt" 2>/dev/null | \
 while IFS= read -r LINE; do
-    [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" && -n "${LINE}" ]] && continue
     [[ -z "${LINE}" ]] && echo ${2} && echo "${1} ${2}" >>${3}releases.txt && break
+    [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" ]] && continue
     [[ -n "${LINE}" ]] && name=$(echo "${data}" | cut -d " " -f 1)
     [[ -n "${LINE}" ]] && url=$(echo "${data}" | cut -d " " -f 2)
     if [[ -n "${LINE}" && "${2}" != "${url}" ]]; then
