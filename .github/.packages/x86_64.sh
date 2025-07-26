2580d91c93af87fdebc20c02ccee0e122b385226
3wlh <1094890624@qq.com>
2025-07-26 12:23:49 +0800

    up

---
+++ .github/.packages/x86_64.sh
@@ -0,0 +1,7 @@
+Segmentation "https://dl.openwrt.ai/releases/24.10/packages/x86_64/kiddin9/" \
+"${1}/packages/x86_64" \
+"luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel \
+taskd luci-lib-xterm luci-lib-taskd luci-app-store"
+Passwall "x86_64" "24.10" "${1}"
+Openlist2 "x86_64" "${1}"
+Nikki "x86_64" "${1}"
