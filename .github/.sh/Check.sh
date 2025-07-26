14210d58fd7a494940a41d3fc461a4e93eeb9a52
3wlh <1094890624@qq.com>
2025-07-26 10:03:43 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cat "${3}releases.txt" 2>/dev/null | \
+[[ !-f ${3}releases.txt ]] && touch ${3}releases.txt
+cat "${3}releases.txt" | \
 while IFS= read -r LINE; do
     [[ -z "${LINE}" ]] && echo ${2} && echo "${1} ${2}" >>${3}releases.txt && break
     [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" ]] && continue
