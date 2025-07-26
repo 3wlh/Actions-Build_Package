593aa0605c41c10a3fb006b365a177f860fd8906
3wlh <1094890624@qq.com>
2025-07-26 11:38:13 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -3,7 +3,7 @@ test -f "${3}releases.txt" || touch "${3}releases.txt"
 function modify(){
 cat "${3}" | \
 while IFS= read -r LINE; do
-    [[ -z "$(echo "${LINE}" | grep -Eo "^Package:${1}.*")" ]] && continue
+    [[ -z "$(echo "${LINE}" | grep -Eo "^${1}.*")" ]] && continue
     url=$(echo "${LINE}" | cut -d " " -f 2)
     if [[ "${2}" != "${url}" ]]; then
         echo "update"
