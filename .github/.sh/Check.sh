f5dd8f72c080418f564aa2c5aeecb9b037bb89df
3wlh <1094890624@qq.com>
2025-07-26 09:48:24 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-data=""
 cat "${3}releases.txt" | \
 while IFS= read -r LINE; do
-    [[ -z "${1}" || -z "${2}" || -z "${LINE}" ]] && continue
-    [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" ]] && continue
-    data=${LINE}
-    echo ${data}
+    [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" && -n "${LINE}" ]] && continue
+    [[ -z "${LINE}" ]] && echo ${2} && echo "${1} ${2}" >>${3}releases.txt && break
+    [[ -n "${LINE}" ]] && name=$(echo "${data}" | cut -d " " -f 1)
+    [[ -n "${LINE}" ]] && url=$(echo "${data}" | cut -d " " -f 2)
+    if [[ -n "${LINE}" && "${2}" != "${url}" ]]; then
+        echo ${2}
+        sed -i "s|${url}|${2}|" "${3}releases.txt"
+    fi
 done
-echo ${data}
-[[ -z "${data}" ]] && echo ${2} && echo "${1} ${2}" >>${3}releases.txt 
-[[ -n "${data}" ]] && name=$(echo "${data}" | cut -d " " -f 1)
-[[ -n "${data}" ]] && url=$(echo "${data}" | cut -d " " -f 2)
-if [[ -n "${data}" && "${2}" != "${url}" ]]; then
-    echo ${2}
-    sed -i "s|${url}|${2}|" "${3}releases.txt"
-fi
+
