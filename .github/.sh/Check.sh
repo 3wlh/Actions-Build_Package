eb9e00409627b16e37bdedc21a54494d42cb19a4
3wlh <1094890624@qq.com>
2025-07-26 09:37:48 +0800

    Update Check.sh

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -4,7 +4,8 @@ cat "${3}releases.txt" | \
 while IFS= read -r LINE; do
     [[ -z "${1}" || -z "${2}" || -z "${LINE}" ]] && continue
     [[ -z "$(echo "${LINE}" | grep -Eo "${1}")" ]] && continue
-    data=${LINE} 
+    data=${LINE}
+    echo ${data}
 done
 echo ${data}
 [[ -z "${data}" ]] && echo ${2} && echo "${1} ${2}" >>${3}releases.txt 
@@ -12,5 +13,5 @@ echo ${data}
 [[ -n "${data}" ]] && url=$(echo "${data}" | cut -d " " -f 2)
 if [[ -n "${data}" && "${2}" != "${url}" ]]; then
     echo ${2}
-    sed -i "s|${url}|${2}|" ${3}releases.txt
+    sed -i "s|${url}|${2}|" "${3}releases.txt"
 fi
