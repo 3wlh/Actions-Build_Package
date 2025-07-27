b98ec8b0686cd80d706801b89f6acf2d324c2b7c
3wlh <1094890624@qq.com>
2025-07-28 01:45:39 +0800

    Update Delete.sh

--- .github/.shell/Delete.sh
+++ .github/.shell/Delete.sh
@@ -3,6 +3,6 @@ PACKAGES_PATH="${1}"
 PACKAGES_NAME=(${2})
 for PREFIX in "${PACKAGES_NAME[@]}"; do
 	file=$(find ${PACKAGES_PATH} -type f -name "*${PREFIX}*.[ia]pk")
-	[[ -f ${file} ]] && rm -f ${file} && \
+	[[ -f ${file} ]] && sudo rm -f ${file} && \
 	echo "$(date '+%Y-%m-%d %H:%M:%S') - 【$(basename ${file})】插件删除."
 done
