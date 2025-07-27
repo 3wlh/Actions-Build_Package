8186c05edfe7fb1d9c10bed912fa2f1eb4705e66
3wlh <1094890624@qq.com>
2025-07-28 01:25:08 +0800

    Update Download.sh

--- .github/.shell/Download.sh
+++ .github/.shell/Download.sh
@@ -3,7 +3,7 @@ echo "Downloading ${1}"
 if [[ "$(du -b $(pwd)/$(basename ${1}) 2>/dev/null | awk '{print $1}')" -ge "512" ]]; then
     echo "######################################################################## 100.0%"
 else
-    find ${2} -type f -name "*$(basename ${1} | cut -d "_" -f1)*" -exec rm -f {} \;
-    curl -# -L --fail "${1}" -o "$(pwd)/$(basename ${2})"
+    find ${2} -type f -name "*$(echo "$(basename ${1})" | cut -d "_" -f1)*" -exec rm -f {} \;
+    curl -# -L --fail "${1}" -o "$(pwd)/$(basename ${1})"
     # #wget -qO "$(pwd)/packages/diy_packages/$(basename $Download_URL)" "${Download_URL}" --show-progress
 fi
