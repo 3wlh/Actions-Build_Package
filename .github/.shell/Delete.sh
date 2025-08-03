893ae7c16643b62d0ae2fa77747fa2e95589c7bf
3wlh <1094890624@qq.com>
2025-08-03 17:33:47 +0800

    Update Delete.sh

--- .github/.shell/Delete.sh
+++ .github/.shell/Delete.sh
@@ -4,8 +4,10 @@ PACKAGES_PATH="${1}"
 PACKAGES_NAME=(${2})
 for PREFIX in "${PACKAGES_NAME[@]}"; do
 	file=$(find ${PACKAGES_PATH} -type f -name "*${PREFIX}*.[ia]pk")
-	[[ -f ${file} ]] && sudo rm -f ${file} && [[ -f ${file} ]] || \
-	echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 【$(basename ${file})】插件删除.\e[0m"
+	for name in $file; do
+		[[ -f ${name} ]] && sudo rm -f ${name} && [[ -f ${name} ]] || \
+		echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;31m - 【$(basename ${name})】插件删除.\e[0m"
+	done
 done
 }
 App_list=$(find "${1}" -type f -name "*.[ia]pk" -exec basename {} \;| cut -d '_' -f1)
