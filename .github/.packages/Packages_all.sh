e29cd7718ce14c8fc4dc62fc863579ba344ba5c3
3wlh <1094890624@qq.com>
2025-08-09 16:05:20 +0800

    Update Packages_all.sh

--- .github/.packages/Packages_all.sh
+++ .github/.packages/Packages_all.sh
@@ -7,7 +7,9 @@ Openlist2 "${ARCH}" "${PACKAGES_PATH}"
 Nikki "${ARCH}" "${PACKAGES_PATH}"
 # Socat "${ARCH}" "${PACKAGES_PATH}"
 Segmentation "${PACKAGES_PATH}" "https://dl.openwrt.ai/releases/24.10/packages/${ARCH}/kiddin9/" \
-"luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel luci-app-socat"
+"luci-app-unishare unishare webdav2 luci-app-v2ray-server"
+Segmentation "${PACKAGES_PATH}" "https://istore.istoreos.com/repo/all/nas_luci/" \
+"sunpanel luci-app-sunpanel luci-app-socat luci-i18n-socat-zh-cn"
 Segmentation "${PACKAGES_PATH}" "https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/${ARCH}/luci/" \
 "luci-app-homeproxy luci-i18n-homeproxy-zh-cn luci-app-ramfree luci-i18n-ramfree-zh-cn 
 luci-app-argon-config luci-i18n-argon-config-zh-cn luci-theme-argon"
