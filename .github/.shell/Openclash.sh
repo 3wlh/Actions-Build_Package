cac50414d3f8c1e893d4d453bb5b7e19d6f98d81
3wlh <1094890624@qq.com>
2025-12-16 21:28:36 +0800

    up

--- .github/.shell/Openclash.sh
+++ .github/.shell/Openclash.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 Time="$(date '+%Y-%m-%d %H:%M:%S')" && mkdir -p "$(pwd)/openclash" && DIR="$(pwd)/openclash"
-IPK_url="https://github.com/3wlh/Actions-Build_Package/releases/download/2025.12.15_001200/luci-app-openclash-ninja_0.47.028_all.ipk"
+Data="$(curl -s https://api.github.com/repos/3wlh/Actions-Build_Package/releases/tags/GitHub-Actions_luci-app-openclash-ninja)"
+ipk_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*luci-app-openclash-ninja.*"' | cut -d '"' -f 4)"
+[[ -z "$(Check "openclash" "${ipk_url}" "${2}/.")" ]] && echo -e "${Time}\e[1;32m - 【openclash】插件无更新.\e[0m" && exit
 echo "${Time} - 下载 luci-app-openclash-ninja ..."
-curl -# -L --fail "${IPK_url}" -o "${DIR}/$(basename ${IPK_url})"
-if [[ "$(du -b "${DIR}/$(basename ${IPK_url})" 2>/dev/null | awk '{print $1}')" -le "20000" ]]; then
-	echo -e "${Time}\e[1;31m - 【${DIR}/$(basename ${IPK_url})】下载失败.\e[0m"
+curl -# -L --fail "${ipk_url}" -o "${DIR}/$(basename ${ipk_url})"
+if [[ "$(du -b "${DIR}/$(basename ${ipk_url})" 2>/dev/null | awk '{print $1}')" -le "20000" ]]; then
+	echo -e "${Time}\e[1;31m - 【${DIR}/$(basename ${ipk_url})】下载失败.\e[0m"
 fi
 Delete "${DIR}" "${2}"
