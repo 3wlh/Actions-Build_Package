da2b859e125b95c90e35e891e096bd2ff584e345
3wlh <1094890624@qq.com>
2025-12-20 17:52:10 +0800

    Update napcatapi.js

--- luci-app-napcatapi/htdocs/luci-static/resources/view/napcatapi.js
+++ luci-app-napcatapi/htdocs/luci-static/resources/view/napcatapi.js
@@ -75,7 +75,7 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'path_config', _('Config File Path'));
-		o.default = '/etc/napcat/napcat.yaml';
+		o.default = '/etc/napcatapi/napcatapi.yaml';
 		o.rmempty = true;
 		
 		o = s.option(form.Value, 'pwd_config', _('Decrypt KEY'));
