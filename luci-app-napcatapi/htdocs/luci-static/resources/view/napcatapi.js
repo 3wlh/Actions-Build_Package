cd7321ded5e51e33b26b5296d50015cc7a851711
3wlh <1094890624@qq.com>
2025-12-20 21:27:38 +0800

    up

--- luci-app-napcatapi/htdocs/luci-static/resources/view/napcatapi.js
+++ luci-app-napcatapi/htdocs/luci-static/resources/view/napcatapi.js
@@ -4,46 +4,42 @@
 'require rpc';
 'require uci';
 'require view';
-
-var callServiceList = rpc.declare({
-	object: 'service',
-	method: 'list',
-	params: ['name'],
-	expect: { '': {} }
-});
+'require fs';
 
 function getServiceStatus() {
-	return L.resolveDefault(callServiceList('napcat-api'), {}).then(function (res) {
-		var isRunning = false;
-		try {
-			isRunning = res['napcat-api']['instances']['instance1']['running'];
-		} catch (e) { }
+		let isRunning = false;
 		return isRunning;
-	});
 }
 
 function renderStatus(isRunning, port) {
 	var spanTemp = '<span style="color:%s"><strong>%s %s</strong></span>';
-	var renderHTML;
+	let renderHTML;
 	if (isRunning) {
-		var button = String.format('&#160;<a class="btn cbi-button" href="http://%s:%s" target="_blank" rel="noreferrer noopener">%s</a>',
+		let button = String.format('&#160;<a class="btn cbi-button" href="http://%s:%s" target="_blank" rel="noreferrer noopener">%s</a>',
 			window.location.hostname, port, _('Open Web Interface'));
-		renderHTML = spanTemp.format('green', _('NapCat'), _('RUNNING')) + button;
+		renderHTML = spanTemp.format('green', _('NapCat API'), _('RUNNING')) + button;
 	} else {
-		renderHTML = spanTemp.format('red', _('NapCat'), _('NOT RUNNING'));
+		renderHTML = spanTemp.format('red', _('NapCat API'), _('NOT RUNNING'));
 	}
 
 	return renderHTML;
 }
 
 return view.extend({
-	load: function() {
-		return uci.load('napcat-api');
+	load: async function () {
+		const promises = await Promise.all([
+			L.resolveDefault(fs.stat('/var/run/napcat-api.pid'), null),
+			uci.load('napcat-api')
+		]);
+	const data = {
+			isRunning: promises[0],
+			conf: promises[1]
+		};
+	return data;
 	},
-
-	render: function(data) {
-		var m, s, o;
-		var webport = (uci.get(data, 'config', 'port') || '5663');
+	render(data) {
+		let m, s, o;
+		let webport = (uci.get(data, 'config', 'port') || '5663');
 
 		m = new form.Map('napcat-api', _('NapCat-API'),
 			_('NapCat Robot call the API..'));
@@ -52,10 +48,10 @@ return view.extend({
 		s.anonymous = true;
 		s.render = function () {
 			poll.add(function () {
-				return L.resolveDefault(getServiceStatus()).then(function (res) {
-					var view = document.getElementById('service_status');
-					view.innerHTML = renderStatus(res, webport);
-				});
+				return Promise.resolve().then(function() {
+					let view = document.getElementById('service_status');
+					view.innerHTML = renderStatus(data.isRunning, webport);
+        });
 			});
 
 			return E('div', { class: 'cbi-section', id: 'status_bar' }, [
@@ -84,7 +80,7 @@ return view.extend({
 		o.rmempty = true;
 		
 		o = s.option(form.Value, 'online_config', _('Online Config URL'));
-		o.default = 'http://';
+		o.default = '';
 		o.rmempty = true;
 		o.datatype = 'or(url,empty)';
 
