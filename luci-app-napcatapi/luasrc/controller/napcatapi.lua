3a7b1719a3b7e0edd261e88f9d05c1ebb0a00026
3wlh <1094890624@qq.com>
2026-01-04 21:06:23 +0800

    up

--- luci-app-napcatapi/luasrc/controller/napcatapi.lua
+++ luci-app-napcatapi/luasrc/controller/napcatapi.lua
@@ -4,8 +4,13 @@ function index()
 	if not nixio.fs.access("/etc/config/napcatapi") then
 		return
 	end
-	entry({"admin", "services", "napcatapi"}, cbi("napcatapi/napcatapi"), _("NapCat Api"), 20).dependent = true
+	entry({"admin", "services", "napcatapi"}, firstchild(), _("NapCat Api"), 90).dependent = true
 	entry({"admin", "services","napcatapi_status"}, call("Run_status"))
+	-- 注册菜单 
+	entry({"admin", "services", "napcatapi", "settings"}, cbi("napcatapi/napcatapi"), _("Settings"), 10).leaf = true
+	entry({"admin", "services", "napcatapi", "edit"}, template("napcatapi/edit"), _("Edit"), 20).leaf = true
+	entry({"admin", "services", "napcatapi", "napcat"}, template("napcatapi/napcat"), _("NapCat"), 30).leaf = true
+	entry({"admin", "services", "napcatapi", "log"}, template("napcatapi/log"), _("Log"), 40).leaf = true
 end
 
 function Run_status()
@@ -21,3 +26,4 @@ function Run_status()
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(status)
 end
+
