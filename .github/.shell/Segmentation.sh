5a3dfe13564383f90b6a007085f632d331a2b8d4
3wlh <1094890624@qq.com>
2026-03-11 14:57:23 +0800

    更新 Segmentation.sh

--- .github/.shell/Segmentation.sh
+++ .github/.shell/Segmentation.sh
@@ -13,6 +13,8 @@ while IFS= read -r LINE; do
             fi
             Download_URL="${PACKAGES_URL}${FILE}"
             if [[ ! -f "${PACKAGES_PATH}/${FILE}" ]];then
+                # 删除旧PACKAGES
+                find ${PACKAGES_PATH} -type f -name "*${PREFIX}*" -exec rm -f {} \;
                 Download "${Download_URL}" "${PACKAGES_PATH}"
             else
                 echo -e "$(date '+%Y-%m-%d %H:%M:%S')\e[1;32m - (${FILE})插件无更新.\e[0m"
