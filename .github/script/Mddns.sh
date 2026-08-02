f37cb740867b93c75f875ef56e085f532c94cf18
3wlh <3wlh@3wlh.cn>
2026-08-02 23:09:20 +0800

    更新 Mddns.sh

--- .github/script/Mddns.sh
+++ .github/script/Mddns.sh
@@ -4,7 +4,7 @@ PACKAGES_NAME=("luci-app-mddns-script" "mddns-aliyun-script" "mddns-cloudflare-s
 PACKAGES_ARCH="${1}"
 PACKAGES_PATH="${2}"
 function dl(){
-Time="$(date '+%Y-%m-%d %H:%M:%S')" && mkdir -p "$(pwd)/${FILE}" && DIR="$(pwd)/${FILE}"
+Time="$(date '+%Y-%m-%d %H:%M:%S')" && mkdir -p "$(pwd)/Mddns" && DIR="$(pwd)/Mddns"
 IPK_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*'${FILE}'.*'${PACKAGES_ARCH}'.*"' | cut -d '"' -f 4)"
 [[ -z "$(Check "${FILE}" "${IPK_url}" "${PACKAGES_PATH}")" ]] && echo -e "${Time} - \e[1;32m【${FILE}】插件无更新.\e[0m" && return
 echo "${Time} - 下载 ${FILE} ..."
