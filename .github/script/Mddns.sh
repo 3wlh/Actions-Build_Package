b6fee4551eddb7c78dbe6a400ab8bf43d068b30a
3wlh <3wlh@3wlh.cn>
2026-08-02 23:23:12 +0800

    更新 Mddns.sh

--- .github/script/Mddns.sh
+++ .github/script/Mddns.sh
@@ -5,7 +5,7 @@ PACKAGES_ARCH="${1}"
 PACKAGES_PATH="${2}"
 function dl(){
 Time="$(date '+%Y-%m-%d %H:%M:%S')" && mkdir -p "$(pwd)/Mddns" && DIR="$(pwd)/Mddns"
-IPK_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*'${FILE}'.*'${PACKAGES_ARCH}'.*"' | cut -d '"' -f 4)"
+IPK_url="$(echo "${Data}" | grep -Eo '"browser_download_url":\s*".*'${FILE}_'.*_'${PACKAGES_ARCH}'.*"' | cut -d '"' -f 4)"
 [[ -z "$(Check "${FILE}" "${IPK_url}" "${PACKAGES_PATH}")" ]] && echo -e "${Time} - \e[1;32m【${FILE}】插件无更新.\e[0m" && return
 echo "${Time} - 下载 ${FILE} ..."
 curl -# -L --fail "${IPK_url}" -o "${DIR}/$(basename ${IPK_url})"
@@ -14,7 +14,7 @@ if [[ "$(du -b "${DIR}/$(basename ${IPK_url})" 2>/dev/null | awk '{print $1}')" 
 fi
 Delete "${DIR}" "${PACKAGES_PATH}"
 }
-
+curl -s https://api.github.com/repos/3wlh/OpenWrt_Packages/releases/tags/GitHub-Actions_luci-app-mddns-script | grep -Eo '"browser_download_url":\s*".*luci-app-mddns-script.*all.*"' | cut -d '"' -f 4
 Data="$(curl -s ${PACKAGES_URL})"
 for FILE in "${PACKAGES_NAME[@]}"; do
     if [[ -n ${FILE} ]]; then
