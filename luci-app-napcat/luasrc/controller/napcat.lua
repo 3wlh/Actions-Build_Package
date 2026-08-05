572e33d8877ae25edcd9828cef330f282df617ce
3wlh <3wlh@3wlh.cn>
2026-08-05 13:18:56 +0800

    更新 napcat.lua

--- luci-app-napcat/luasrc/controller/napcat.lua
+++ luci-app-napcat/luasrc/controller/napcat.lua
@@ -2,22 +2,27 @@ local name = "napcat"
 module("luci.controller." .. name, package.seeall) 
 local api = require "luci.model.cbi."..name..".api.download"
 
--- 二进制名与下载源 (action_index 渲染 + action_download 回退共用)
-local bin_file = "docker-web"
-local download_url = "https://github.com/3wlh/Actions-Source/releases/download/GitHub-Actions_docker_web/" .. bin_file
-
 function index() 
     entry({"admin", "services", name}, call("action_index"), _("NapCat"), 90).dependent = true
     entry({"admin", "services", name, "download_exec"}, call("action_download")).leaf = true
     entry({"admin", "services", name, "app"}, call("app"), _("Settings"), 10).leaf = true 
 end 
 
+-- 二进制文件名与下载地址
+local bin_file = docker-web
+local cnb_url = string.format("https://cnb.cool/3wlh/Build-File/-/releases/download/GitHub-Actions_%s/%s-%%s",bin_file ,bin_file)
+local github_url = string.format("https://github.com/3wlh/Actions-Source/releases/download/GitHub-Actions_%s/%s-%%s",bin_file ,bin_file)
+
+function Get_Url()
+	return cnb_url
+end
+
 -- 主入口: 实时检查二进制, 存在则跳转设置页, 不存在则显示下载页面
 function action_index()
 	if api.installed(bin_file) then
-		luci.http.redirect(luci.dispatcher.build_url("admin", "system", name, "settings"))
+		luci.http.redirect(luci.dispatcher.build_url("admin", "system", name, "app"))
 	else
-		local info = api.arch_info(bin_file, download_url)
+		local info = api.arch_info(bin_file, Get_Url())
 		luci.template.render(name.."/download", {
 			Name = name,
 			arch = info.arch,
@@ -31,7 +36,7 @@ end
 
 -- 下载二进制: 服务端用控制器常量推导路径/URL, 调 api.download
 function action_download()
-	local info = api.arch_info(bin_file, download_url)
+	local info = api.arch_info(bin_file, Get_Url())
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(api.download(info.bin_path, info.bin_url))
 end
@@ -100,7 +105,7 @@ function app()
     if os.execute(cmd) then 
         luci.template.render(name.."/app", { 
             Port = port,
-            -- Token = token
+            Token = token
         }) 
     end
 end
