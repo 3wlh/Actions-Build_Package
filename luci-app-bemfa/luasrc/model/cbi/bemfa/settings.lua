1dd4bec7f930a6efb1eb28c4ad1a6f4598c450a5
3wlh <3wlh@3wlh.cn>
2026-08-22 23:53:48 +0800

    更新 settings.lua

--- luci-app-bemfa/luasrc/model/cbi/bemfa/settings.lua
+++ luci-app-bemfa/luasrc/model/cbi/bemfa/settings.lua
@@ -28,7 +28,7 @@ init_config()
 local m, s, o
 m = Map(name, _("Configuration"), 
     _("A professional IoT device access and management platform, providing stable and reliable persistent TCP connections and MQTT protocol support.")
-    .. "<br/>" .. _("Official reference") .. ": <a href='https://cloud.bemfa.com/docs/src/' target='_blank'>Bemfa</a>")
+    .. "<br/>" .. _("Official reference") .. ": <a href='https://cloud.bemfa.com/tcp/index.html' target='_blank'>Bemfa</a>")
 
 m.apply_on_parse = true -- 解析阶段立即写入配置文件
 m.on_after_commit = function(self)
@@ -48,23 +48,23 @@ s.anonymous = true
 -- 启用开关
 s:option(Flag, "enabled", _("Enable")).rmempty = false
 
--- 端口配置
+-- 配置令牌
 o = s:option(Value, "key", _("UserKey"))
 o.password = true
 o.rmempty = true
-o.description = _("User Private Key")
+o.description = _("Please enter your user token.")
 
--- 配置文件路径
+-- 配置订阅
 o = s:option(Value, "topic", _("Topic"))
 o.rmempty = true
 o.datatype = "string"
 o.description = _('Please enter the subscription topics, separated by |.');
 
--- 解密密钥
+-- 配置消息
 o = s:option(Value, "msg", _("Msg"))
 o.default = "on"
 o.rmempty = true
-o.description = _('Filter (empty = no filter, use | to separate)');
+o.description = _('Filter keywords (empty = no filter; if set, only messages matching any keyword are kept; separate with |)');
 
 -- 渲染表单
 return m
